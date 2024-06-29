62cf19ad6fa6cc107be989cc75f666e58fae9261
bill-BKH <bill.bkh@gmail.com>
2024-06-29 21:08:59 +0330

    pagination half backed

--- product/views.py
+++ product/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 def product(request):
-    return render(request,'product/product.html')
+    data = Product.objects.all()
+    paginator = Paginator(data,3)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request,'product/product.html',{'products':page_obj,'paginator':paginator})
 
