cf38b4ec7082bfcab9560c67e380e9cb6f536db2
bill-BKH <bill.bkh@gmail.com>
2024-07-06 21:09:28 +0330

    register page basic logic imlemented

---
+++ account/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(label='ایمیل')
+    password = forms.CharField(label='پسورد',widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='تایید پسورد')
+
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise ValidationError('رمز عبور و تکرار آن باهم مطابق نیستند')
+        else:
+            return password
--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
+from .models import User
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user_email = request.POST.get('email')
+            user = User.objects.filter(email__iexact=user_email).first()
+            print(user)
+            if user == None:
+                print('no user with that email')
+            else:
+                print('this email si not unique')
+                form.add_error('email','ایمیل داده شده تکراری است')
+                return render(request, 'account/register.html',{'form':form})
+        else:
+            return render(request, 'account/register.html',{'form':form})
+        
+    return render(request, 'account/register.html',{'form':RegisterForm})
