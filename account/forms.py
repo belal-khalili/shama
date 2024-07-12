a41909511c35abe9568c327e0cf7ee5b79e69a5b
aria1hz <aryahajizadeh0@gmail.com>
2024-07-12 15:01:46 -0700

    ACCOUNT Styles Updated

--- account/forms.py
+++ account/forms.py
@@ -2,9 +2,23 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class RegisterForm(forms.Form):
-    email = forms.EmailField(label='ایمیل')
-    password = forms.CharField(label='پسورد',widget=forms.PasswordInput())
-    confirm_password = forms.CharField(label='تایید پسورد')
+    email = forms.EmailField(label='ایمیل',
+    widget=forms.EmailInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'لطفا ایمیل خود را وارد نمایید'})
+    )
+    password = forms.CharField(label='پسورد',
+    widget=forms.PasswordInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'لطفا گذرواژه خود را وارد نمایید'
+    })
+                               )
+    confirm_password = forms.CharField(label='تایید پسورد',
+    widget=forms.PasswordInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'لطفا گذرواژه خود را تکرار نمایید'
+    })
+)
 
 
     def clean_confirm_password(self):
@@ -17,5 +31,10 @@ class RegisterForm(forms.Form):
         
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(label='ایمیل')
-    password = forms.CharField(label='پسورد',widget=forms.PasswordInput())
+    email = forms.EmailField(label='ایمیل',
+    widget=forms.EmailInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'لطفا ایمیل خود را وارد نمایید'}))
+    password = forms.CharField(label='پسورد',widget=forms.PasswordInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'لطفا گذرواژه خود را وارد نمایید'}))
