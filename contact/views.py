22bd0cb5a7f93590d125eba585450636d5b1e627
Bardiaa85 <bardiacodes85@gmail.com>
2024-07-13 10:31:19 +0330

    img added to contact app

--- contact/views.py
+++ contact/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
-from .forms import ContactForm
-from .models import Contact
-
-# Create your views here.
+from .forms import ContactModelForm
 def contact(request):
     if request.method == 'POST':
         print(request.POST)
-        form = ContactForm(request.POST)
+        form = ContactModelForm(request.POST , request.FILES)
         if form.is_valid():
-            subject = request.POST.get('subject')
-            full_name = request.POST.get('full_name')
-            email = request.POST.get('email')
-            phone_number = request.POST.get('phone_number')
-            text = request.POST.get('text')
-
-            new_contact = Contact(subject=subject,full_name=full_name,email=email,phone_number=phone_number,text=text)
-            new_contact.save()
+            form.save()
         else:
             return render(request,'contact/contact.html',{'form':form})
-
-    return render(request,'contact/contact.html',{'form':ContactForm})
+    return render(request,'contact/contact.html',{'form':ContactModelForm})
