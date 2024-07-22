a181ea9c8957256911f370e13f8621aee4ed04b9
aria1hz <aryahajizadeh0@gmail.com>
2024-07-22 07:56:24 -0700

    Page problem Fixed

--- category/views.py
+++ category/views.py
@@ -5,10 +5,10 @@ from django.core.paginator import Paginator
 
 
 
-# def category_page(request,cat_name):
-#     data = Product.objects.filter(category__link=cat_name)
-#     paginator= Paginator(data,4)
-#     page_number=request.GET.get("page")
-#     this_page=paginator.get_page(page_number)
-#     print(data)
-#     return render(request, 'category/category.html',{"this_page" : this_page , "paginator" : paginator} )
+def category_page(request,cat_name):
+    data = Product.objects.filter(category__link=cat_name)
+    paginator= Paginator(data,2)
+    page_number=request.GET.get("page")
+    this_page=paginator.get_page(page_number)
+    print(data)
+    return render(request, 'category/category.html',{"this_page" : this_page , "paginator" : paginator, "cat_name" : cat_name} )
