e2e2e7da92641f3ff9d69d5bbb8fa8ca60675eda
erfannir <erfannirooyar@gmail.com>
2024-07-22 16:09:52 +0330

    user folder and template problems solved

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,3 +5,29 @@ from django.http import HttpResponse
 # this funciton is temporary and should not be continued
 def show_user_info(request):
     return HttpResponse(request.user.password)
+
+
+def profile(request):
+    return render(request, 'user_profile/profile.html')
+
+def sefareshha(request):
+    return render(request, 'user_profile/sefareshha.html')
+    
+def darkhastmarjui(request):
+    return render(request, 'user_profile/darkhastmarjui.html')
+
+def favoritelist(request):
+    return render(request, 'user_profile/favoritelist.html')
+
+def nazarat(request):
+    return render(request, 'user_profile/nazarat.html')
+
+def adresses(request):
+    return render(request, 'user_profile/adresses.html')
+
+def bazdidakhir(request):
+    return render(request, 'user_profile/bazdidakhir.html')
+
+def userinfo(request):
+    return render(request, 'user_profile/userinfo.html')
+
